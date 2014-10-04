4764f4791831140577ba7f2165292ddab6269d7d
Joshua <echojoshchen@gmail.com>
2014-10-04 16:54:24 -0700

    csv file support

--- mapper/scripts/map.js
+++ mapper/scripts/map.js
@@ -1,20 +1,23 @@
 var SantaRosa = [38.438710, -122.716763,"Santa Rosa"];
-var markers = ['images/marker1.png', 'images/marker2.png', 'images/marker3.png', 'images/marker4.png', 'images/markerE.png', 
-               'images/markerF.png', 'images/markerG.png', 'images/markerH.png', 'images/markerI.png', 'images/markerJ.png',
-               'images/markerK.png', 'images/markerL.png', 'images/markerM.png', 'images/markerN.png', 'images/markerO.png',
-               'images/markerP.png', 'images/markerQ.png', 'images/markerR.png', 'images/markerS.png', 'images/markerT.png',
-               'images/markerU.png', 'images/markerV.png', 'images/markerW.png', 'images/markerX.png', 'images/markerY.png', 'images/markerZ.png'];
-var addresses = ['1838 Bella Vista Way, Santa Rosa, CA', '3500 Parker Hill Road, Santa Rosa, CA', 'Crown Hill Rd, Santa Rosa, CA'];
+var addresses = [];
 var coords = [];
 var curr_stop = null;
 var map;
+var mapRead = false;
 
 $(document).ready(function(){
-  geocodeAddress(0);
-  createMap();
-
-  $(".active").click(function() {
-    refreshMap();
-  });
+    // The event listener for the file upload
+    $("#txtFileUpload").change(handleTerritory);
+    $("#refresh").click(refreshMap);
+    
+	createMap();
+	//geocodeAddress(0);
 });
 
+// Callback function for when a territory file is selected
+function handleTerritory(event)
+{
+	uploadFile(event);
+	//geocodeAddress(0);
+	refreshMap();
+}
