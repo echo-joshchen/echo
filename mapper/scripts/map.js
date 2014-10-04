a7fcc8eb5b8f0096ba24f008586c75b7245edb84
Joshua <echojoshchen@gmail.com>
2014-10-04 00:48:16 -0700

    mapper fixes

--- mapper/scripts/map.js
+++ mapper/scripts/map.js
@@ -1,18 +1,17 @@
-var Bakersfield = [35.424536,-120.325521,"Bakersfield"];
-var SanFrancisco = [37.796763,-122.422234,"San Francisco"];
-var SanDiego = [32.717977,-117.158993,"San Diego"];
-var MBAcquarium = [36.618051,-121.902061,"Monterey Bay Aquarium"];
+var SantaRosa = [38.438710, -122.716763,"Santa Rosa"];
 var markers = ['images/marker1.png', 'images/marker2.png', 'images/marker3.png', 'images/marker4.png', 'images/markerE.png', 
                'images/markerF.png', 'images/markerG.png', 'images/markerH.png', 'images/markerI.png', 'images/markerJ.png',
                'images/markerK.png', 'images/markerL.png', 'images/markerM.png', 'images/markerN.png', 'images/markerO.png',
                'images/markerP.png', 'images/markerQ.png', 'images/markerR.png', 'images/markerS.png', 'images/markerT.png',
                'images/markerU.png', 'images/markerV.png', 'images/markerW.png', 'images/markerX.png', 'images/markerY.png', 'images/markerZ.png'];
-var addresses = ['1838 Bella Vista Way, Santa Rosa, CA', '3500 Parker Hill Road, Santa Rosa, CA', '2500 Monticito Blvd, Santa Rosa, CA'];
+var addresses = ['1838 Bella Vista Way, Santa Rosa, CA', '3500 Parker Hill Road, Santa Rosa, CA', 'Crown Hill Rd, Santa Rosa, CA'];
 var coords = [];
 var curr_stop = null;
+var map;
 
 $(document).ready(function(){
-  beforeCreateMap();
+  geocodeAddress(0);
+  createMap();
 
   $(".active").click(function() {
     refreshMap();
--- mapper/scripts/mapfunctions.js
+++ mapper/scripts/mapfunctions.js
@@ -4,187 +4,66 @@ This file has Google Maps API functions.
 
 **/
 
-function geocodeAddresses() {
+function geocodeAddress(index) {
   // Geocode all addresses
-  for (var i = 0; i < addresses.length; i++) {
+  if (index < addresses.length)
+  {
 	GMaps.geocode({
-      address: addresses[i],
+      address: addresses[index],
       callback: function(results, status) {
         if (status == 'OK') {
           var latlng = results[0].geometry.location;
-          coords[i] = [latlng.lat(), latlng.lng(), addresses[i]];
+          coords[index] = [latlng.lat(), latlng.lng(), addresses[index]];
           refreshMap();
+          geocodeAddress(index + 1);
         }
       }
     });
   }
+  else
+  {
+	  refreshMap();
+  }
 }
 
-// Creates the initial map from SF to SD.
+// Creates the initial map at Santa Rosa
 function createMap() {
   
   map = new GMaps({
     el: '#map',
-    lat: startLocation[0],
-    lng: startLocation[1],
-    zoom: 7,
+    lat: SantaRosa[0],
+    lng: SantaRosa[1],
+    zoom: 9
   });
-
-  // start
-  map.addMarker({
-    lat: startLocation[0],
-    lng: startLocation[1],
-    title: "Start Location: " + startLocation[2],
-    icon: "images/marker1.png"
-  });
-
-  if (endLocation.length != 0) {
-
-    map.setCenter((startLocation[0]+endLocation[0])/2, (startLocation[1]+endLocation[1])/2);
-
-    map.addMarker({
-      lat: endLocation[0],
-      lng: endLocation[1],
-      title: "End Location: " + endLocation[2],
-      icon: "images/marker2.png"
-    });
-
-    map.drawRoute({
-      origin: startLocation,
-      destination: endLocation,
-      travelMode: 'driving',
-      strokeColor: '#CC0000',
-      strokeOpacity: 0.6,
-      strokeWeight: 6
-    });
-  }
-  else {
-    endLocation = startLocation;
-  }
-
-  map.setContextMenu({
-    control: 'map',
-    options: [
-    {
-      title: 'Search here',
-      name: 'search_here',
-      action: function(e) {
-        search([e.latLng.lat(), e.latLng.lng()], "");
-        $("#sidebar a:last").tab("show");
-      }
-    },
-    {
-      title: 'Center here',
-      name: 'center_here',
-      action: function(e) {
-        this.setCenter(e.latLng.lat(), e.latLng.lng());
-      }
-    }]
-  });
-
-  if (startLocation != endLocation) {
-    fitMap();
-  }
-  else {
-    map.setZoom(9);
-  }
 
   return map;
 }
 
-// Renders the current route
-function renderRoute() {
-  map.cleanRoute();
-  renderStep(0);
-}
-
-function renderStep(i) {
-  if (i == 0 && path.length == 0) {
-    map.drawRoute({
-      origin: startLocation.slice(0, 2),
-      destination: endLocation.slice(0, 2),
-      travelMode: 'driving',
-      strokeColor: '#CC0000',
-      strokeOpacity: 0.6,
-      strokeWeight: 6
-    });
-  } else if (i == 0) {
-    map.drawRoute({
-      origin: startLocation.slice(0, 2),
-      destination: path[i].slice(0, 2),
-      travelMode: 'driving',
-      strokeColor: '#CC0000',
-      strokeOpacity: 0.6,
-      strokeWeight: 6
-    });
-    setTimeout(function() {renderStep(i+1)}, 50);
-    //renderStep(i+1);
-  } else if (i == path.length) {
-    map.drawRoute({
-      origin: path[i-1].slice(0, 2),
-      destination: endLocation.slice(0, 2),
-      travelMode: 'driving',
-      strokeColor: '#CC0000',
-      strokeOpacity: 0.6,
-      strokeWeight: 6
-    });
-  } else {
-    map.drawRoute({
-      origin: path[i-1].slice(0, 2),
-      destination: path[i].slice(0, 2),
-      travelMode: 'driving',
-      strokeColor: '#CC0000',
-      strokeOpacity: 0.6,
-      strokeWeight: 6
-    });
-    setTimeout(function() {renderStep(i+1)}, 50);
-    //renderStep(i+1);
-  }
-}
-
-// Adds back the markers for the route, after the search markers are deleted.
-// I didn't find a way to delete certain markers, so I'm resorting to this method.
-function addRouteMarkers() {
-
-  // Add start location
-  map.addMarker({
-    lat: startLocation[0],
-    lng: startLocation[1],
-    title: "Start Location: " + startLocation[2],
-    icon: "images/marker1.png"
-  });
-
-  // Add end location
-  if (endLocation != startLocation) {
-    map.addMarker({
-      lat: endLocation[0],
-      lng: endLocation[1],
-      title: "End Location: " + endLocation[2],
-      icon: "images/marker2.png"
-    });
-  }
-
-  // Add all path markers
-  for (var i = 1; i < addresses.length - 1; i++) {
-    map.addMarker({
-      lat: addresses[i][0],
-      lng: addresses[i][1],
-      title: addresses[i][2],
-      icon: markers[i],
-      infoWindow: {
-      content: "<p>" + path[i][2] + "</p><p><input onclick='search([" + path[i][0] + "," + path[i][1] + "], \"\")'" + " type='button' value='Search Nearby'></p>" + 
-        "<span id='marker' class='delete' onclick='cancelStopMarker(\"" + path[i][2] + "\")'><img src='images/cancel.png' alt='cancel' /></span>"
-      }
-    });
+// Add the markers
+function setAddressMarkers() {
+  // Add all markers
+  for (var i = 0; i < addresses.length; i++) {
+	if (coords[i] != null)
+	{
+		map.addMarker({
+		  lat: coords[i][0],
+		  lng: coords[i][1],
+		  title: addresses[i],
+		  icon: markers[i]
+		});
+	}
   }
 
   fitMap();
 }
 
 function fitMap() {
-  var places = [new google.maps.LatLng(startLocation[0], startLocation[1]), new google.maps.LatLng (endLocation[0], endLocation[1])];
-  for (var i = 0; i < path.length; i++) {
-    places.push(new google.maps.LatLng(path[i][0], path[i][1]));
+  var places = [];
+  for (var i = 0; i < coords.length; i++) {
+	  if (coords[i] != null)
+	  {
+	  	places.push(new google.maps.LatLng(coords[i][0], coords[i][1]));
+	  }
   }
   var bounds = new google.maps.LatLngBounds ();
   //  Go through each...
@@ -205,5 +84,5 @@ function fitMap() {
 function refreshMap() {
   fitMap();
   map.removeMarkers();
-  addRouteMarkers();
+  setAddressMarkers();
 }
