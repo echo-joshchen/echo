5d6d00296e43c44757b1bedf793cb2b647da5f66
Joshua <echojoshchen@gmail.com>
2014-10-04 17:37:17 -0700

    missing files

---
+++ mapper/scripts/csvimport.js
@@ -0,0 +1,67 @@
+var data = null;
+
+// Method that checks that the browser supports the HTML5 File API
+function browserSupportFileUpload()
+{
+    var isCompatible = false;
+    if (window.File && 
+        window.FileReader && 
+        window.FileList && 
+        window.Blob) 
+    {
+		isCompatible = true;
+    }
+    return isCompatible;
+}
+
+// Method that reads and processes the selected file
+function uploadFile(evt)
+{
+	if (!browserSupportFileUpload()) 
+	{
+		alert('The File APIs are not fully supported in this browser!');
+    } 
+    else 
+    {
+        var file = evt.target.files[0];
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            var csvData = event.target.result;
+            data = $.csv.toArrays(csvData);
+            if (data && data.length > 0) 
+            {
+                //alert('Imported -' + data.length + '- rows.');
+                processGeocodedData();
+            } 
+            else 
+            {
+                alert('No data to import!');
+            }
+        };
+        reader.onerror = function() {
+            alert('Unable to read ' + file.fileName);
+        };
+        reader.readAsText(file);
+	}
+}
+
+// Process territory data
+function processData()
+{
+    for (var i = 1; i < 5; i++)
+    {
+        var address = data[i][4] + " " + data[i][5] + " " + 
+            data[i][6] + ", " + data[i][7] + ", " + data[i][8];
+        addresses[i - 1] = address;
+    }
+}
+
+// Process territory data that already has coordinates
+function processGeocodedData()
+{
+    for (var i = 1; i < data.length; i++)
+    {
+        coords[i-1] = [data[i][21], data[i][22]];
+    }
+}
+
